738c60fe340728c2d1d9b2dde259e80fb679078f
Sander <sander@recras.nl>
2021-03-05 14:34:33 +0100

    Replace demo

--- demo/app.js
+++ demo/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('multirangeDemo', ['vds.multirange']);
+angular.module('timerangeDemo', ['recras.timerange']);
--- demo/controllers.js
+++ demo/controllers.js
@@ -1,22 +1,22 @@
 'use strict';
 
-angular.module('multirangeDemo')
-  .controller('DemoCtrl', function ($scope, vdsMultirangeViews) {
-
+angular.module('timerangeDemo').controller('DemoCtrl', function ($scope, recrasTimerangeViews) {
     // populate some data
     $scope.rangeArray = [
-     { value: 0.20, name: 'Clock In',color:'red' },
-     { value: 0.40, name: 'Start Break',color:'green' },
-     { value: 0.66, name: 'End Break' ,color:'blue'},
-     { value: 0.80, name: 'Clock Out' ,color:'yellow' },
-	 { value: 0.90, name: 'Clock Out' ,color:'cyan' },
-	 { value: 0.50, name: 'Clock Out' ,color:'brown' }
+        {value: 0.20, color: 'red'},
+        {value: 0.40, color: 'green'},
+        {value: 0.66, color: 'blue'},
+        {value: 0.80, color: 'yellow'},
+        {value: 0.90, color: 'cyan'},
+        {value: 0.50, color: 'brown'},
     ];
 
-    $scope.views = vdsMultirangeViews.TIME;
+    $scope.views = recrasTimerangeViews.TIME;
 
     $scope.add = function () {
-      $scope.rangeArray.push({ value: parseFloat($scope.entry), name: 'Test entry',color:$scope.entryColor });
+        $scope.rangeArray.push({
+            value: parseFloat($scope.entry),
+            color: $scope.entryColor
+        });
     };
-
-  });
+});
